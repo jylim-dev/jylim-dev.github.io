af47a5e77809f5a8c6625a4a320222b3d8eb6bba
jylim-dev <jylim.dev@gmail.com>
2025-03-17 21:44:57 -0700

    add project

--- src/features/projects/Container.jsx
+++ src/features/projects/Container.jsx
@@ -1,7 +1,31 @@
+import PageTitle from "../shared/components/PageTitle";
+import useProject from "./useProject";
+
 const ProjectsContainer = () => {
+  const { projects } = useProject();
+
   return (
-    <main>
-      <h1>I participated in..</h1>
+    <main className="max-w-4xl mx-auto p-4 space-y-8">
+      <PageTitle>I participated in..</PageTitle>
+
+      <div className="flex flex-wrap gap-4">
+        <div className="overflow-hidden w-full h-64">
+          {projects.map((project, index) => (
+            <a
+              key={index}
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:cursor-pointer"
+            >
+              <img
+                src={project.src}
+                className="w-full h-full object-cover rounded-xl transition-transform hover:scale-105 duration-700"
+              />
+            </a>
+          ))}
+        </div>
+      </div>
     </main>
   );
 };
---
+++ src/features/projects/useProject.jsx
@@ -0,0 +1,8 @@
+import Project1 from "/img/project1.png";
+
+const useProject = () => {
+  const projects = [{ src: Project1, link: "https://bsideu.ca" }];
+  return { projects };
+};
+
+export default useProject;
